2de0eaf1d2b2412b50ebb4962944c6540949b462
Uwe Dauernheim <uwe@klarna.com>
2012-02-26 15:33:23 +0100

    Add functioncalls

--- src/grapherl.erl
+++ src/grapherl.erl
@@ -24,7 +24,7 @@
 %%   <dt>`open'</dt><dd>Command to run on resulting file as a
 %%     string. This command will with the output file generated from
 %%     `dot' as input.</dd>
-%%   <dt>`verbose'</dt><dd>Make `xref' verdbose. Default is `false'.</dd>
+%%   <dt>`verbose'</dt><dd>Make `xref' verbose. Default is `false'.</dd>
 %%   <dt>`warnings'</dt><dd>Make `xref' print warnings. Default is `false'</dd>
 %% </dl>
 -module(grapherl).
@@ -32,11 +32,15 @@
 -copyright("Erlang Solutions Ltd.").
 -author("Adam Lindberg <eproxus@gmail.com>").
 
--export([main/1]).
--export([applications/2]).
--export([applications/3]).
--export([modules/2]).
--export([modules/3]).
+-export([ main/1
+        , applications/2
+        , applications/3
+        , modules/2
+        , modules/3
+        , functioncalls/2
+        , functioncalls/3
+        , functioncalls/4
+        ]).
 
 -ifdef(TEST).
 -include("grapherl_tests.hrl").
@@ -50,33 +54,37 @@
 main(Args) ->
     {ok, {Flags, _Rest} = Options} = getopt:parse(options(), Args),
     case lists:member(help, Flags) of
-        true  -> print_options(), halt(0);
+        true  -> print_options(), quit(0);
         false -> run(Options)
     end.
 
 run({Options, [Dir, Target]}) ->
     case get_mode(Options) of
         {app, RestOpt} -> run(applications, [Dir, Target, RestOpt]);
-        {mod, RestOpt} -> run(modules, [Dir, Target, RestOpt])
+        {mod, RestOpt} -> run(modules, [Dir, Target, RestOpt]);
+        {funcalls, RestOpt} -> run(functioncalls, [Dir, Target, RestOpt])
     end;
 run({_Options, _Other}) ->
-    print_options(), halt(1).
+    print_options(), quit(1).
 
 get_mode(Options) ->
-    case proplists:split(Options, [app, mod]) of
+    case proplists:split(Options, [app, mod, funcalls]) of
         {[[app], []], Rest} -> {app, Rest};
-        {[[], [mod]], Rest} -> {mod, Rest}
+        {[[], [mod]], Rest} -> {mod, Rest};
+        {[[], [], [funcalls]], Rest} -> {funcalls, Rest}
     end.
 
 options() ->
     [{help, $h, "help", undefined,
       "Display this help text"},
-     {mod, $m, "modules", undefined,
-      "Analyse module dependencies (mutually exclusive)"},
      {app, $a, "applications", undefined,
       "Analyse application dependencies (mutually exclusive)"},
+     {mod, $m, "modules", undefined,
+      "Analyse module dependencies (mutually exclusive)"},
+     {funcalls, $f, "functioncalls", undefined,
+      "Analyse function call dependencies (mutually exclusive)"},
      {type, $t, "type", string,
-      "Output file type (also deduced from file name)"}].
+      "Output file type (otherwise deduced from file name)"}].
 
 print_options() ->
     getopt:usage(options(), filename:basename(escript:script_name()),
@@ -86,15 +94,10 @@ print_options() ->
 
 run(Fun, Args) ->
     try apply(?MODULE, Fun, Args) of
-        ok ->
-            halt(0);
-        {error, Error} ->
-            io:format("grapherl: error: ~p~n", [Error]),
-            halt(2)
+        ok             -> quit(0);
+        {error, Error} -> quit(2, Error)
     catch
-        error:type_not_specified ->
-            io:format("grapherl: error: File type not specified~n"),
-            halt(2)
+        error:type_not_specified -> quit(2, 'File type not specified')
     end.
 
 %% @equiv applications(Dir, Target, [{type, png}])
@@ -106,20 +109,20 @@ applications(Dir, Target) ->
 %% `Dir' is the library directory of the release you want to graph. `Target'
 %5 is the target filename (without extension).
 applications(Dir, Target, Options) ->
-    check_dot(),
+    check_for_dot(),
     try
         initialize_xref(?MODULE, Options),
         ok(xref:add_release(?MODULE, Dir, {name, ?MODULE})),
         Excluded = ifc(proplists:is_defined(include_otp, Options),
                        [], otp_apps())
             ++ proplists:get_value(excluded, Options, []),
-        {ok, Results} = xref:q(?MODULE, "AE"),
+        Results = ok(xref:q(?MODULE, "AE")),
         Relations = [uses(F, T) ||
                         {F, T} <- Results,
                         F =/= T,
                         not lists:member(F, Excluded),
                         not lists:member(T, Excluded)],
-        create(["node [shape = tab];"] ++ Relations, Target, Options),
+        create(["node [shape = tab];\n"] ++ Relations, Target, Options),
         stop_xref(?MODULE)
     catch
         throw:Error ->
@@ -128,9 +131,9 @@ applications(Dir, Target, Options) ->
     end.
 
 
-%% @equiv application(App, Target, [{type, png}])
+%% @equiv modules(Dir, Target, [{type, png}])
 modules(Dir, Target) ->
-    modules(Dir, Target, []).
+    modules(Dir, Target, [{type, png}]).
 
 %% @doc Generate a module dependency graph for an application.
 %%
@@ -143,23 +146,56 @@ modules(Dir, Target) ->
 %% a direct source for .beam files.
 modules(Dir, Target, Options) ->
     %% TODO: Thickness of arrows could be number of calls?
-    check_dot(),
+    check_for_dot(),
     try
         initialize_xref(?MODULE, Options),
-        Path = get_path(Dir),
+        Path = get_ebin_path(Dir),
         ok(xref:add_directory(?MODULE, Path)),
         Modules = case ok(xref:q(?MODULE, "AM")) of
                       [] -> throw({error, no_modules_found});
                       Else  -> Else
                   end,
-        Query = "ME ||| ["
-            ++ string:join(["'" ++ atom_to_list(M) ++ "'" || M <- Modules], ",")
-            ++ "]",
-        {ok, Results} = xref:q(?MODULE, Query),
+        Query = fmt("ME ||| ~w", [Modules]),
+        Results = ok(xref:q(?MODULE, Query)),
         Relations = [uses(F, T) || {F, T} <- Results, F =/= T],
-        create(["node [shape = box];"]
-               ++ [["\"" ++ atom_to_list(M) ++ "\"", $;] || M <- Modules]
+        create(["node [shape = box];\n"]
+               ++ [fmt("\"~w\";", [M]) || M <- Modules]
                ++ Relations, Target, Options),
+        stop_xref(?MODULE)
+    catch
+        throw:Error ->
+            stop_xref(?MODULE),
+            Error
+    end.
+
+%% @equiv functioncalls(ModuleFile, [], Target)
+functioncalls(ModuleFile, Target) ->
+    functioncalls(ModuleFile, [], Target).
+
+%% @equiv functioncalls(ModuleFile, Excludes, Target, [{type,png}])
+functioncalls(ModuleFile, Excludes, Target) ->
+    functioncalls(ModuleFile, Excludes, Target, [{type,png}]).
+
+%% @doc Generate a function call dependency graph for a Module.
+%%
+%% `Module' is the target module. `Target' is the target filename
+%% (without extension).
+%%
+%% All function calls from the module `Module' will be in the graph.
+%% The option `no_ebin' will, if set to true or just included as an
+%% atom, use the `Dir' directory as a direct source for .beam files.
+functioncalls(ModuleFile, Excludes, Target, Options) ->
+    %% TODO: Thickness of arrows could be number of calls?
+    check_for_dot(),
+    try
+        initialize_xref(?MODULE, Options),
+        ok(xref:add_module(?MODULE, ModuleFile)),
+        Module = erlang:list_to_atom(filename:basename(ModuleFile, ".beam")),
+        Query = fmt("E | ~w", [Module]),
+        Results = ok(xref:q(?MODULE, Query)),
+        Relations = [uses(F, T, Module) || {F, {TM,_,_}=T} <- Results, F =/= T,
+                                           not lists:member(TM, Excludes)],
+        create(["node [shape = box];\n"] ++ Relations, Target, Options),
         stop_xref(?MODULE)
     catch
         throw:Error ->
@@ -171,7 +207,7 @@ modules(Dir, Target, Options) ->
 %% Internal Functions
 %%==============================================================================
 
-get_path(Dir) ->
+get_ebin_path(Dir) ->
     case filelib:wildcard(filename:join(Dir, "*.beam")) of
         []     -> filename:join(Dir, "ebin");
         _Beams  -> Dir
@@ -206,15 +242,22 @@ type_from_filename(Filename) ->
     end.
 
 file(Lines) ->
-    ["digraph application_graph {", Lines, "}"].
+    fmt("digraph application_graph {~n~s}~n", [Lines]).
+
+uses({FromM,FromF,FromA}, {ToM,ToF,ToA}, FromM) ->
+    fmt("\"~w:~w/~w\" [style=filled, fillcolor=yellow]"
+        "\"~w:~w/~w\" -> \"~w:~w/~w\";~n",
+        [FromM, FromF, FromA, FromM, FromF, FromA, ToM, ToF, ToA]);
+uses({FromM,FromF,FromA}, {ToM,ToF,ToA}, _) ->
+    fmt("\"~w:~w/~w\" -> \"~w:~w/~w\";~n",
+        [FromM, FromF, FromA, ToM, ToF, ToA]).
 
 uses(From, To) ->
-    ["\"" ++ atom_to_list(From) ++ "\"", " -> ",
-     "\"" ++ atom_to_list(To) ++ "\"", $;].
+    fmt("  \"~w\" -> \"~w\";~n", [From, To]).
 
 create(Lines, Target, Options) ->
     case dot(file(Lines), Target, get_type(Options, Target)) of
-        {"", File}    ->
+        {"", File} ->
             case proplists:get_value(open, Options) of
                 undefined -> ok;
                 Command -> os:cmd(Command ++ " " ++ File), ok
@@ -223,7 +266,7 @@ create(Lines, Target, Options) ->
             {error, hd(string:tokens(Error, "\n"))}
     end.
 
-check_dot() ->
+check_for_dot() ->
     case os:cmd("dot -V") of
         "dot " ++ _ ->
             ok;
@@ -232,13 +275,15 @@ check_dot() ->
     end.
 
 dot(File, Target, Type) ->
-    TmpFile = string:strip(os:cmd("mktemp -t " ?MODULE_STRING ".XXXX"), both, $\n),
+    TmpFile = make_tempfile(),
     ok = file:write_file(TmpFile, File),
     TargetName = add_extension(Target, Type),
-    Result = os:cmd(io_lib:format("dot -T~p -o~p ~p",
-                                  [Type, TargetName, TmpFile])),
+    Result = os:cmd(fmt("dot -T~p -o~p ~p", [Type, TargetName, TmpFile])),
     ok = file:delete(TmpFile),
     {Result, TargetName}.
+
+make_tempfile() ->
+    string:strip(os:cmd("mktemp -t " ?MODULE_STRING ".XXXX"), right, $\n).
 
 add_extension(Target, Type) ->
     case filename:extension(Target) of
@@ -247,8 +292,21 @@ add_extension(Target, Type) ->
     end.
 
 otp_apps() ->
-    {ok, Apps} = file:list_dir(filename:join(code:root_dir(), "lib")),
+    Apps = ok(file:list_dir(filename:join(code:root_dir(), "lib"))),
     [list_to_atom(hd(string:tokens(A, "-"))) || A <- Apps].
+
+quit(ExitCode) ->
+    erlang:halt(ExitCode).
+
+quit(ExitCode, Msg) ->
+    quit(ExitCode, "~p", [Msg]).
+
+quit(ExitCode, Fmt, Data) ->
+    io:format(standard_error, "~p: error: "++Fmt++"~n", [?MODULE|Data]),
+    timer:sleep(500),
+    erlang:halt(ExitCode).
+
+fmt(Fmt, Data) -> lists:flatten(io_lib:format(Fmt, Data)).
 
 ok({ok, Result}) -> Result;
 ok(Error)        -> throw(Error).
